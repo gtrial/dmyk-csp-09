b777e68018bb1f2fc4b61759d8f30f697eed6fb4
Dmytro Kostiuchenko <dmyk@nuuday.dk>
2021-06-29 15:42:52 +0300

    Task 2

--- task2/Program.cs
+++ task2/Program.cs
@@ -6,7 +6,45 @@ namespace task2
     {
         private static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(GC.GetTotalMemory(true));
+            MyClass.MakeSomeGarbage();
+            Console.WriteLine(GC.GetTotalMemory(true));
+            Console.WriteLine("Insert memory limit:");
+
+            if (int.TryParse(Console.ReadLine(), out var memoryLimit))
+            {
+                MyClass.CheckLimits(memoryLimit);
+            }
+            else
+            {
+                Console.WriteLine("Invalid limit value");
+            }
+        }
+    }
+
+    internal class MyClass
+    {
+        private const long MaxGarbage = 1000;
+
+        public static void MakeSomeGarbage()
+        {
+            Version vt = null;
+
+            for (var i = 0; i < MaxGarbage; i++)
+            {
+                vt = new Version();
+            }
+
+            if (vt is not null) Console.WriteLine(vt.ToString());
+        }
+
+        public static void CheckLimits(int memoryLimit)
+        {
+            var totalMemory = GC.GetTotalMemory(true);
+            if (totalMemory > memoryLimit)
+            {
+                Console.WriteLine($"Memory allocated {totalMemory} is over the limit of {memoryLimit}");
+            }
         }
     }
 }
